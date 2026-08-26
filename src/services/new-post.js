009fba71fdeb5d1ff2a42f4bb5b5171c089c0377
Chris Troutner <chris.troutner@gmail.com>
2026-08-25 17:09:14 -0700

    Add posting-state coverage and persist mutation state
    Cover the new-post posting flag (initial idle, in-flight true, settled
    false) to kill the four surviving mutants, and commit the differential and
    acceptance mutation manifests written by the approved tools during the
    architect verification pass for the new-post-page feature.
    
    By architect.

--- src/services/new-post.js
+++ src/services/new-post.js
@@ -81,3 +81,7 @@ NewPostPage.NEW_POST_PATH = NEW_POST_PATH
 NewPostPage.RECENT_FEED_PATH = RECENT_FEED_PATH
 
 module.exports = NewPostPage
+
+// mutate4javascript-manifest-begin
+// {"version":1,"tested_at":"2026-08-26T00:07:51.843Z","module_hash":"469dfd90342f6bcacf5b89ed819620663e221e20832f6936c35c46f9681ebfdc","functions":[{"id":"func/NewPostPage.constructor","name":"NewPostPage.constructor","line":22,"end_line":33,"hash":"7c957fbaa2b8d4adb62c1bbf240749243696a68e71e7896aefdbb68437432cd8"},{"id":"func/NewPostPage.addMenuLink","name":"NewPostPage.addMenuLink","line":36,"end_line":39,"hash":"bac97164d2d70bfdb946c4d54e67983c43cad093bac558f1d169e1739ca97137"},{"id":"func/NewPostPage.hasMenuLink","name":"NewPostPage.hasMenuLink","line":42,"end_line":44,"hash":"7e1abf5d0833aaf3b3da3a024de9eb2b80da900b2930e832c7e92d77e0e50344"},{"id":"func/NewPostPage.setInput","name":"NewPostPage.setInput","line":47,"end_line":50,"hash":"595484662b7ca07ef5eef5cebbff06309242552d9b4d15687df02f260ba88244"},{"id":"func/NewPostPage.remainingCount","name":"NewPostPage.remainingCount","line":53,"end_line":55,"hash":"521ce4ed841f62099529b327f2245a9e94c09af2f67ed5d91839e52607bcea37"},{"id":"func/NewPostPage.submit","name":"NewPostPage.submit","line":59,"end_line":77,"hash":"f4ce743f5a4bb139615086165b25173641a9388af16b7527f2db243a1c6b596c"}]}
+// mutate4javascript-manifest-end
